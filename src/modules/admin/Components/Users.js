820e73a5ee31195261a4ab7d60ede1bf0ea09f54
abdul-kodir2020 <mounirouabdul40@gmail.com>
2024-01-19 18:02:56 +0100

    ajout liste étudaiants

--- src/modules/admin/Components/Users.js
+++ src/modules/admin/Components/Users.js
@@ -1,9 +1,63 @@
 import React from 'react'
+import pp from '../../../images/avatar_25.jpg'
 
 function Users() {
   return (
     <div>
-        <h4 className='fw-bold'>Utilisateurs</h4>
+        <div className='d-flex justify-content-between'>
+          <h4 className='fw-bold'>Etudiants</h4>
+          <button className='btn btn-dark'><i class="fa-regular fa-square-plus"></i> Ajouter</button>
+        </div>
+        <div className='container-list rounded-3 shadow p-3 mt-4'>
+          <h6 className='fw-bold m-0'>Liste des étudiants</h6>
+          <hr className='my-3'/>
+          <table class="table table-hover table-borderless">
+            <thead className=''>
+              <tr >
+                <th scope="col"><input class="form-check-input" type="checkbox" value="" aria-label="Checkbox for following text input"/></th>
+                <th scope="col">Etudiant</th>
+                <th scope="col">Nom d'utilisateur</th>
+                <th scope="col">Email</th>
+                <th scope="col">Spécialité</th>
+                <th scope="col">Classe</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row"><input class="form-check-input" type="checkbox" value="" aria-label="Checkbox for following text input"/></th>
+                {/* <td className='td-profil-pic'><img alt='pp' src={pp}/></td> */}
+                <td className='td-profil-pic'><img alt='pp' src={pp}/> Mounirou abdul kodir</td>
+                <td>kodir2023</td>
+                <td>mounirouabdul40@gmail.com</td>
+                <td>DAD</td>
+                <td>DAD - A</td>
+                <td><button className='btn'><i class="fa-solid fa-ellipsis"></i></button></td>
+              </tr>
+              <tr>
+                <th scope="row"><input class="form-check-input" type="checkbox" value="" aria-label="Checkbox for following text input"/></th>
+                {/* <td className='td-profil-pic'><img alt='pp' src={pp}/></td> */}
+                <td className='td-profil-pic'><img alt='pp' src={pp}/> Mounirou abdul kodir</td>
+                <td>kodir2023</td>
+                <td>mounirouabdul40@gmail.com</td>
+                <td>DAD</td>
+                <td>DAD - A</td>
+                <td><button className='btn'><i class="fa-solid fa-ellipsis"></i></button></td>
+              </tr>
+              <tr>
+                <th scope="row"><input class="form-check-input" type="checkbox" value="" aria-label="Checkbox for following text input"/></th>
+                {/* <td className='td-profil-pic'><img alt='pp' src={pp}/></td> */}
+                <td className='td-profil-pic'><img alt='pp' src={pp}/> Mounirou abdul kodir</td>
+                <td>kodir2023</td>
+                <td>mounirouabdul40@gmail.com</td>
+                <td>DAD</td>
+                <td>DAD - A</td>
+                <td><button className='btn'><i class="fa-solid fa-ellipsis"></i></button></td>
+              </tr>
+              
+            </tbody>
+          </table>
+        </div>
     </div>
   )
 }

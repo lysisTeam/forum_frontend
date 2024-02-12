d48128b42aeab4b364b68700603a2729e2f336b8
abdul-kodir2020 <mounirouabdul40@gmail.com>
2024-02-12 19:22:44 +0100

    update page chat

--- src/modules/admin/Layouts/Chat.js
+++ src/modules/admin/Layouts/Chat.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from 'react'
 import image from '../../../images/Robot Cosmonaut.jpg'
+import avatar from "../../../images/avatar.jpg"
 
 function Chat({currentRoom}) {
   const apiUrl = process.env.REACT_APP_API_URL
 
   useEffect(()=>{
-    
+    setTimeout(() => {
+      var objDiv = document.querySelector('.section-messages');
+        if (objDiv) {
+          objDiv.scroll({
+            top: objDiv.scrollHeight,
+            behavior: "smooth",
+          });
+        }
+      }, 1);
 
 
   },[currentRoom])
@@ -13,6 +22,23 @@ function Chat({currentRoom}) {
   const resizeTextArea = (e) =>{
     e.target.style.height = 'auto'; // Réinitialisez la hauteur à auto
     e.target.style.height = (e.target.scrollHeight) + 'px'; // Ajustez la hauteur en fonction du contenu
+
+    console.log(document.querySelector('.chat-input-section').clientHeight);
+    console.log(e.target.scrollHeight);
+
+    setTimeout(() => {
+      document.querySelector('.section-chat .chat-open .chat-bottom-bar').style.height = (document.querySelector('.chat-input-section').clientHeight + 43) + 'px'
+    }, 1);
+
+    setTimeout(() => {
+      var objDiv = document.querySelector('.section-messages');
+        objDiv.scroll({
+          top: objDiv.scrollHeight,
+          behavior: "smooth",
+        });
+    }, 2);
+    
+
   }
   return (
     <div className='section-chat'>
@@ -43,35 +69,35 @@ function Chat({currentRoom}) {
             <div className='container section-messages'>
               {/*message entrant */}
               <div className='message-entrant'>
-                <div className='px-3 py-2 rounded'>
-                    Bonjour <br></br>qsdcsfdvc effdv qsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdv
-                    <span className='time'>11:50</span>
+                <img alt='user-avatar' src={avatar} />
+                <div>
+                  <span className='name'>Abdul mounirou</span>
+                  <div className='px-3 py-2 rounded message-container'>
+                      Bonjour
+                      <span className='time'>11:50</span>
+
+                      <span data-icon="tail-in" class="coin">
+                        <svg viewBox="0 0 8 13" height="13" width="8" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 8 13"><title>tail-in</title><path opacity="0.13" fill="#0000000" d="M1.533,3.568L8,12.193V1H2.812 C1.042,1,0.474,2.156,1.533,3.568z"></path><path fill="currentColor" d="M1.533,2.568L8,11.193V0L2.812,0C1.042,0,0.474,1.156,1.533,2.568z"></path></svg>
+                      </span>
+                  </div>
                 </div>
               </div>
+
 
               {/*message sortant */}
               <div className='message-sortant'>
-                <div className='px-3 py-2 rounded'>
-                    Bonjour <br></br>qsdcsfdvc effdv qsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdv
+                <span className='name'>Moi</span>
+                <div className='px-3 py-2 rounded message-container'>
+                    Bonjour
                     <span className='time'>11:50</span>
+
+                    <span data-icon="tail-out" class="coin">
+                      <svg viewBox="0 0 8 13" height="13" width="8" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 8 13"><title>tail-out</title><path opacity="0.13" d="M5.188,1H0v11.193l6.467-8.625 C7.526,2.156,6.958,1,5.188,1z"></path><path fill="currentColor" d="M5.188,0H0v11.193l6.467-8.625C7.526,1.156,6.958,0,5.188,0z"></path></svg>
+                    </span>
                 </div>
               </div>
 
-               {/*message entrant */}
-               <div className='message-entrant'>
-                <div className='px-3 py-2 rounded'>
-                    Bonjour <br></br>qsdcsfdvc effdv qsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdv
-                    <span className='time'>11:50</span>
-                </div>
-              </div>
-
-              {/*message sortant */}
-              <div className='message-sortant'>
-                <div className='px-3 py-2 rounded'>
-                    Bonjour <br></br>qsdcsfdvc effdv qsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdvqsdcsfdvc effdv
-                    <span className='time'>11:50</span>
-                </div>
-              </div>
+        
 
               
 

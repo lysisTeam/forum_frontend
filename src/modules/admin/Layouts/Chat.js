f2364f18253644cc09c07f077e27c013dd350ea7
abdul-kodir2020 <mounirouabdul40@gmail.com>
2024-02-16 18:48:50 +0100

    add response to message, fix some issues about z-index, add options section message etc...

--- src/modules/admin/Layouts/Chat.js
+++ src/modules/admin/Layouts/Chat.js
@@ -4,6 +4,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import ImageLetters from './ImageLetters'
 import axios from 'axios'
 import AdminContext from '../Contexts/AdminContext'
+import EmojiPicker  from "emoji-picker-react"
 
 
 function Chat({currentRoom}) {
@@ -13,18 +14,28 @@ function Chat({currentRoom}) {
   const {admin} = useContext(AdminContext)
   const [users, setUsers] = useState([])
 
+  const [showMessageOptions, setShowMessageOptions] = useState(false)
+
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false)
+
   const [texteAEnvoyer, setTexteAEnvoyer] = useState("")
 
+  const [responseTo, setResponseTo] = useState({id: null, message: "", user: ""})
+
   useEffect(()=>{
-    setTimeout(() => {
-      var objDiv = document.querySelector('.section-messages');
-        if (objDiv) {
-          objDiv.scroll({
-            top: objDiv.scrollHeight,
-            behavior: "smooth",
-          });
-        }
-      }, 1);
+    setShowEmojiPicker(false)
+    setTexteAEnvoyer("")
+    setShowMessageOptions(false)
+    setResponseTo({id: null, message: "", user: ""})
+    // setTimeout(() => {
+    //   var objDiv = document.querySelector('.section-messages');
+    //     if (objDiv) {
+    //       objDiv.scroll({
+    //         top: objDiv.scrollHeight,
+    //         behavior: "smooth",
+    //       });
+    //     }
+    //   }, 1);
 
 
     const getMessages = async()=>{
@@ -37,7 +48,6 @@ function Chat({currentRoom}) {
           })
           .then(response =>{
             // console.log(response);
-            console.log(afficherFileMessages(response.data.messages));
             setMessages(afficherFileMessages(response.data.messages))
             document.querySelector("textarea").rows = 1
             setTimeout(() => {
@@ -99,8 +109,28 @@ function Chat({currentRoom}) {
     getAdmins()
     getMessages()
 
+    const handleClose = (e) =>{
+      console.log(e.target);
+      console.log(showMessageOptions);
+      if ((!e.target.closest('.message-options')) && document.querySelector('.message-options.show')) {
+        setShowMessageOptions(false)
+        document.querySelector('.message-options.show').classList.remove('show')
+        document.querySelectorAll('.btn-option').forEach(btn => {
+          btn.style.opacity = '0'
+        })
+      }
+
+    }
+    
+    document.addEventListener('mousedown', handleClose);
+    document.addEventListener('onscroll', handleClose)
+    return () => {
+      document.removeEventListener('mousedown', handleClose);
+    };
+
     
   },[currentRoom, apiUrl])
+
 
   const resizeTextArea = (e) =>{
     e.target.style.height = 'auto'; // Réinitialisez la hauteur à auto
@@ -123,24 +153,13 @@ function Chat({currentRoom}) {
     
   }
 
-  const handleHover = (e) =>{
-    if (e.target.querySelector('.btn-option')) {
-      e.target.querySelector('.btn-option').style.opacity = "1"
-    }
-    
-  }
-
-  const handleHoverLeave = (e) =>{
-    if (e.target.querySelector('.btn-option')) {
-      e.target.querySelector('.btn-option').style.opacity = "0"
-    }
-  }
 
   const sendMessage = async()=>{
     if (texteAEnvoyer) {
       await axios.post(`${apiUrl}/api/message/send-message/${currentRoom._id}/admin`,{
         type: "message",
-        contenue: texteAEnvoyer
+        contenue: texteAEnvoyer,
+        isResponseTo: responseTo.id || null
       },{
         headers: {
           token: localStorage.admin_token
@@ -157,6 +176,7 @@ function Chat({currentRoom}) {
             });
         }, 1);
         setTexteAEnvoyer("")
+        setResponseTo({id: null, message: "", user: ""})
       })
       .catch((error)=>{
         console.log(error);
@@ -208,7 +228,7 @@ function Chat({currentRoom}) {
         }
 
         // Ajouter le message
-        fileMessages.push({ type: message.type, contenue: message.contenue,  id_room: message.id_room, id_user: message.id_user, createdAt: message.createdAt, updatedAt: message.updatedAt});
+        fileMessages.push({ id: message._id ,type: message.type, contenue: message.contenue,  id_room: message.id_room, id_user: message.id_user, isResponseTo: message.isResponseTo ,createdAt: message.createdAt, updatedAt: message.updatedAt});
     });
 
     return fileMessages;
@@ -248,6 +268,29 @@ function Chat({currentRoom}) {
       const mois = (date.getMonth() + 1).toString().padStart(2, '0');
       const annee = date.getFullYear();
       return `${jour}/${mois}/${annee}`;
+  }
+
+  const handleHover = (e) =>{
+    if (e.target.querySelector('.btn-option') && !showMessageOptions) {
+      e.target.querySelector('.btn-option').style.opacity = "1"
+    }
+    
+  }
+
+  const handleHoverLeave = (e) =>{
+    if (e.target.querySelector('.btn-option') && !showMessageOptions) {
+      e.target.querySelector('.btn-option').style.opacity = "0"
+    }
+  }
+
+  const handleEmojiClick = (emoji, event)=>{
+    console.log(emoji);
+    setTexteAEnvoyer(previous => previous + emoji.emoji)
+  }
+
+  const handleClickOption = (e)=>{
+    e.target.parentElement.querySelector('.message-options').classList.add('show')
+    setShowMessageOptions(true)
   }
 
 
@@ -313,13 +356,47 @@ function Chat({currentRoom}) {
                       <div className='px-3 py-2 rounded message-container'>
                           {/* {message.contenue.replace(/\n/g, "<br>")} */}
 
-                          <div dangerouslySetInnerHTML={{ __html: message.contenue.replace(/\n/g, "<br>") }} />
+                          <div className='pl-5'>
+                              {
+                                  message.isResponseTo &&
+                                  <div className='response-section pl-5'>
+                                    <i class="fa-solid fa-reply" style={{fontSize: "0.6rem"}}></i>
+                                    <div className='response' dangerouslySetInnerHTML={{ __html: messages.filter(msg => msg.id === message.isResponseTo)[0].contenue.replace(/\n/g, "<br>") }} />
+                                    <span className='response-name'>
+                                    Envoyé par&nbsp;
+                                    {
+                                      messages.filter(msg => msg.id === message.isResponseTo)[0].id_user !== admin._id ?
+
+                                        users.find(user => user._id === messages.filter(msg => msg.id === message.isResponseTo)[0].id_user)?.nom
+                                      : 
+                                        "moi"
+                                    }
+                                    </span>
+                                    {/* {afficherDateMessage(messages.filter(msg => msg.id === message.isResponseTo)[0].updatedAt)} */}
+                                    <hr className='my-1'></hr>
+                                  </div>
+                                }
+                            
+                            <div dangerouslySetInnerHTML={{ __html: message.contenue.replace(/\n/g, "<br>") }} />
+                          </div>
+
+                          
 
                           <span data-icon="tail-out" class="coin">
                             <svg viewBox="0 0 8 13" height="13" width="8" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 8 13"><title>tail-out</title><path opacity="0.13" d="M5.188,1H0v11.193l6.467-8.625 C7.526,2.156,6.958,1,5.188,1z"></path><path fill="currentColor" d="M5.188,0H0v11.193l6.467-8.625C7.526,1.156,6.958,0,5.188,0z"></path></svg>
                           </span>
 
-                          <button className='btn btn-option'><i class="fa-solid fa-ellipsis-vertical"></i></button>
+                          <div className='section-message-option'>
+                            <button className='btn btn-option' onClick={handleClickOption}><i class="fa-solid fa-ellipsis-vertical pe-none"></i></button>
+                            <div className='message-options shadow'>
+                              <button className=''><i class="fa-regular fa-copy"></i> Copier</button>
+                              <button className='' onClick={()=>{setResponseTo({id: message.id, message: message.contenue, user: "Moi"}); setShowMessageOptions(false); document.querySelector('.message-options.show').classList.remove('show')}}>
+                                <i class="fa-solid fa-reply"></i> Repondre
+                              </button>
+                              <button className=''><i class="fa-solid fa-pen"></i> Modifier</button>
+                              <button className=''><i class="fa-regular fa-trash-can"></i> Supprimer</button>
+                            </div>
+                          </div>
                       </div>
                     </div>
                     :
@@ -336,14 +413,37 @@ function Chat({currentRoom}) {
                         <span className='name fst-italic'>{users.find(user => user._id === message.id_user)?.nom}, {afficherDateMessage(message.updatedAt)}</span>
                         <div className='px-3 py-2 rounded message-container'>
 
-                          <div dangerouslySetInnerHTML={{ __html: message.contenue.replace(/\n/g, "<br>") }} />
+                            <div className='pl-5'>
+                                {
+                                  message.isResponseTo &&
+                                  <div className='response-section pl-5'>
+                                    <i class="fa-solid fa-reply" style={{fontSize: "0.6rem"}}></i>
+                                    <div className='response' dangerouslySetInnerHTML={{ __html: messages.filter(msg => msg.id === message.isResponseTo)[0].contenue.replace(/\n/g, "<br>") }} />
+                                    <span className='response-name'>Envoyé par&nbsp;{users.find(user => user._id === messages.filter(msg => msg.id === message.isResponseTo)[0].id_user)?.nom}</span>
+                                    {/* {afficherDateMessage(messages.filter(msg => msg.id === message.isResponseTo)[0].updatedAt)} */}
+                                    <hr className='my-1'></hr>
+                                  </div>
+                                }
+                                
+                                <div dangerouslySetInnerHTML={{ __html: message.contenue.replace(/\n/g, "<br>") }} />
+                            </div>
                             {/* <span className='time'>11:50</span> */}
 
                             <span data-icon="tail-in" class="coin">
                               <svg viewBox="0 0 8 13" height="13" width="8" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 8 13"><title>tail-in</title><path opacity="0.13" fill="#0000000" d="M1.533,3.568L8,12.193V1H2.812 C1.042,1,0.474,2.156,1.533,3.568z"></path><path fill="currentColor" d="M1.533,2.568L8,11.193V0L2.812,0C1.042,0,0.474,1.156,1.533,2.568z"></path></svg>
                             </span>
-
-                            <button className='btn btn-option'><i class="fa-solid fa-ellipsis-vertical"></i></button>
+                            
+                            <div className='section-message-option'>
+                              <button className='btn btn-option' onClick={handleClickOption}><i class="fa-solid fa-ellipsis-vertical pe-none"></i></button>
+                              <div className='message-options shadow'>
+                                <button className=''><i class="fa-regular fa-copy"></i> Copier</button>
+                                <button className='' onClick={()=>{setResponseTo({id: message.id, message: message.contenue, user: users.find(user => user._id === message.id_user)?.nom}); setShowMessageOptions(false); document.querySelector('.message-options.show').classList.remove('show')}}>
+                                  <i class="fa-solid fa-reply pe-none"></i> 
+                                  Repondre
+                                </button>
+                              </div>
+                            </div>
+                            
                             {/* texte.replace(/\n/g, "<br>"); */}
                         </div>
                       </div>
@@ -351,20 +451,49 @@ function Chat({currentRoom}) {
 
                     
                   ))
+                  
                 }
+                
+                
               
               </div>
             </div>
 
             <div className='chat-bottom-bar'>
-              <div className='container pb-1 d-flex align-items-end'>
-                <div className='chat-input-section rounded shadow p-2'> 
-                  <button className='btn'><i class="fa-regular fa-face-smile"></i></button>
-                  {/* <input type='text' placeholder='Votre message...'/> */}
-                  <textarea id="exampleFormControlTextarea1" rows={1} onChange={(e)=>{resizeTextArea(e); setTexteAEnvoyer(e.target.value)}} value={texteAEnvoyer}></textarea>
-                  <button className='btn'><i class="fa-solid fa-paperclip"></i></button>
-                  <button className='btn btn-send' onClick={()=>sendMessage()}><i class="fa-regular fa-paper-plane pe-none"></i></button>
+              <div className='container pb-1 d-flex flex-column gap-2 justify-content-end'>
+                {
+                  showEmojiPicker &&
+                  <div className='emoji-section'>
+                      <EmojiPicker onEmojiClick={handleEmojiClick} height={400} width={300}/>
+                  </div>
+                }
+                
+                <div className='chat-input-section rounded shadow p-2'>
+                  {
+                    responseTo.id &&
+                    <div className='px-2 chat-input-response'>
+                      <div className='d-flex justify-content-between align-items-center'>
+                        <i class="fa-solid fa-reply" style={{fontSize: "0.6rem"}}></i>
+                        <button className='btn p-0 m-0 border-0' onClick={()=>{setResponseTo({id: null, message: "", user: ""})}}><i class="fa-solid fa-close pe-none" style={{fontSize: "0.7rem"}}></i></button>
+                      </div>
+
+                      <div className='mx-4'>
+                        <p className='m-0 p-0 texte'>{responseTo.message}</p>
+                        <span className=''>Envoyé par {responseTo.user}</span>
+                      </div>
+                      <hr className='my-2'></hr>
+                  </div>
+                  }
+                  <div className='chat-input'>
+                    <button className='btn' onClick={()=>setShowEmojiPicker(previous => !previous)}><i class="fa-regular fa-face-smile pe-none"></i></button>
+                    {/* <input type='text' placeholder='Votre message...'/> */}
+                    <textarea id="exampleFormControlTextarea1" rows={1} onChange={(e)=>{resizeTextArea(e); setTexteAEnvoyer(e.target.value)}} value={texteAEnvoyer}></textarea>
+                    <button className='btn'><i class="fa-solid fa-paperclip"></i></button>
+                    <button className='btn btn-send' onClick={()=>sendMessage()}><i class="fa-regular fa-paper-plane pe-none"></i></button>
+                  </div>
                 </div>
+                
+    
               </div>
               
             </div>
@@ -381,6 +510,7 @@ function Chat({currentRoom}) {
                 <h6>{user.nom}</h6>
               ))
             } */}
+            
             
           </div>
         </div>
